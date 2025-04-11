311d2e9c01a852ebd2e75f8817e43738c34abb1f
lucj8 <lucj8@chinatelecom.cn>
2025-04-11 15:38:09 +0800

    fix:固定用户id

--- app/api/utils/common.ts
+++ app/api/utils/common.ts
@@ -1,16 +1,11 @@
 import { type NextRequest } from 'next/server'
 import { ChatClient } from 'dify-client'
-import { v4 } from 'uuid'
 import { API_KEY, API_URL, APP_ID } from '@/config'
 
-const userPrefix = `user_${APP_ID}:`
-
 export const getInfo = (request: NextRequest) => {
-  const sessionId = request.cookies.get('session_id')?.value || v4()
-  const user = userPrefix + sessionId
   return {
-    sessionId,
-    user,
+    sessionId: 'fixed_session', // 固定session ID
+    user: 'shuzixiangcun',     // 固定用户标识
   }
 }
 
